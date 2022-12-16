9f45e31a8a09734dadee0171d5ff5ac28900e84f
อาลีฟริดฎูวาน อุมา <alifriduwanhellowold17@gmail.com>
2022-12-16 11:05:52 +0700

    เพิ่ม method ให้ IRepo

--- store-client/src/repositories/IRepository.ts
+++ store-client/src/repositories/IRepository.ts
@@ -1,3 +1,8 @@
 export interface IRepository<T> {
   getAll():Promise<T[] | null>
+  get(id: string|number): Promise<T[] | null>
+  create(entity: T): Promise<void>
+  update(entity: Partial<T>): Promise<void>
+  delete(id: string|number): Promise<void>
+
 }
