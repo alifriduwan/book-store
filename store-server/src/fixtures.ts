a0dad6ff781c16a9688336f1e98de50bb9d25ad5
อาลีฟริดฎูวาน อุมา <alifriduwanhellowold17@gmail.com>
2022-12-17 11:24:48 +0700

    เพิ่มข้อมูลใน fixtures ในส่วนของ book

--- store-server/src/fixtures.ts
+++ store-server/src/fixtures.ts
@@ -4,11 +4,17 @@ async function loadFixtures(clearData = false) {
   if(clearData){
       console.warn('clearing data')
       await db('category').del()
+      await db('book').del()
   }
   
   await db.batchInsert('category',[
       {id: 1,title : 'Fantasy'},
       {id: 2,title : 'Biography'}
+  ])
+  await db.batchInsert('book', [
+    { id: 1, title: 'Harry Potter', price: 560, stockAmount: 10, categoryId: 1 },
+    { id: 2, title: 'ALEX FERGUSON', price: 520, stockAmount: 6, categoryId: 2 },
+    { id: 3, title: 'Steve Jobs by Walter', price: 300, stockAmount: 4, categoryId: 2 },
   ])
 }
 
