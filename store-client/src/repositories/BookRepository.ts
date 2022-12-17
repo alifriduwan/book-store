223e038f7d8538076a249320d2a9d1f755c88bd6
อาลีฟริดฎูวาน อุมา <alifriduwanhellowold17@gmail.com>
2022-12-17 15:44:07 +0700

    ปรับให้เรียกใช้ข้อมูล book จาก database

--- store-client/src/repositories/BookRepository.ts
+++ store-client/src/repositories/BookRepository.ts
@@ -1,17 +1,45 @@
 import Book from "../model/Book";
 import { IRepository } from "./IRepository";
+import config from "../config";
+import axios from 'axios';
+
+interface BookFilter {
+  categoryId: string
+}
 
 export class BookRepository implements IRepository<Book> {
-    async getAll(): Promise<Book[] | null> {
-        return [
-            {
-                id: 1, title: 'Harry Potter', price:560, stockAmount: 10,
-                category: {id : 1,title: 'Fantasy'}
-            },
-            {
-                id: 1, title: 'The withcer I', price:399, stockAmount: 6,
-                category: {id: 1,title: 'Fantasy'}
-            }
-        ]
+  urlPrefix = config.remoteRepositoryUrlPrefix
+
+  async getAll(filter: BookFilter): Promise<Book[] | null>{
+    const params = {
+      categoryId: filter.categoryId
     }
+    const result = await axios.get<Book[]>(`${this.urlPrefix}/book`, { params })
+
+    return result.data
+  }
+
+  async get(id: number|string): Promise<Book | null>{
+    const result = await axios.get<Book>(`${this.urlPrefix}/book/${id}`)
+    
+    return result.data
+  }
+
+  async create(entity: Partial<Book>): Promise<void>{
+    const result = await axios.post<Book>(`${this.urlPrefix}/book`, entity)
+    console.log(result.data)
+  }
+
+  async update(entity: Partial<Book>): Promise<void>{
+    const result = await axios.put<Book>(
+      `${this.urlPrefix}/book/${entity.id}`, 
+      entity
+    )
+    console.log(result.data)
+  }
+
+  async delete(id: number|string): Promise<void>{
+    const result = await axios.delete<Book>(`${this.urlPrefix}/book/${id}`)
+    console.log(result.data)
+  }
 }
--- store-client/src/repositories/IRepository.ts
+++ store-client/src/repositories/IRepository.ts
@@ -1,8 +1,7 @@
-export interface IRepository<T> {
-  getAll():Promise<T[] | null>
-  get?(id: string|number): Promise<T[] | null>
-  create?(entity: T): Promise<void>
-  update?(entity: Partial<T>): Promise<void>
-  delete?(id: string|number): Promise<void>
-
+export interface IRepository<T> { 
+  getAll(filter: any|undefined): Promise<T[] | null>; 
+  get(id: string|number): Promise<T | null>;
+  create(entity: T): Promise<void>;
+  update(entity: Partial<T>): Promise<void>;
+  delete(id: string|number): Promise<void>;
 }
